dd851e7b912f696deeb332c1f46e54660ddae7ae
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-16 21:27:04 +0300

    Portfolio.js img

--- portfolio-website/src/sections/Portfolio.js
+++ portfolio-website/src/sections/Portfolio.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import pic from '../assets/pic.jpg';
+import pic from '../assets/images/pic.jpeg';
+import pic1 from '../assets/images/pic1.jpeg';
+import pic2 from '../assets/images/pic2.jpeg';
+import pic3 from '../assets/images/pic3.jpeg';
+import pic4 from '../assets/images/pic4.jpeg';
+import pic5 from '../assets/images/pic5.jpeg';
+import pic6 from '../assets/images/pic6.jpeg';
+import pic7 from '../assets/images/pic7.jpeg';
+import pic8 from '../assets/images/pic8.jpeg';
 
 function Portfolio() {
   return (
@@ -7,8 +15,17 @@ function Portfolio() {
       <h1 className='text-center text-5xl font-bold text-black'>My Works</h1>
       <div id='gallery' className='flex  flex-wrap justify-center items-center gap-8 mt-8'>
         <img src={pic} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
-
+        <img src={pic1} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic2} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic3} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic4} alt='' className='w-[400px] h-[350px] cursor-pointer'/>  
+        <img src={pic5} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic6} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic7} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
+        <img src={pic8} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
       </div>
+
+      <button className='px-10 py-4 bg-orange-500 text-white text-lg font-semibold hover:bg-black mt-6'>VIEW MORE</button>
     </section>
   )
 }
