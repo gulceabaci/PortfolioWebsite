48a6ac178b632937531275ba1f6d35327f78e7c7
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-17 11:11:20 +0300

    Clients.js

--- portfolio-website/src/sections/Clients.js
+++ portfolio-website/src/sections/Clients.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import {Carousel} from 'flowbite-react';
 import client1 from '../assets/images/client1.jpg';
+import client2 from '../assets/images/client2.jpg';
+
 
 function Clients() {
   return (
-    <section id='testimonial' className='w-full flex-col justify-center items-center h-[800px] lg:h-[600px]'>
+    <section id='testimonial' className='w-full flex flex-col justify-center items-center h-[800px] lg:h-[600px]'>
       <Carousel className='w-full lg:w-[80%]' id='clients' slideInterval={2000} indicators={false}>
         <div className='flex items-center justify-between flex-col lg:flex-row w-full px-4 lg:px-32 gap-10'>
           <div id='client-img' className='w-[50%] lg:w-[30%]'>
             <img src={client1} alt='client image' className='w-[600px] h-[300px] lg:w-[350px] lg:h-[400px]' />
           </div>
-          <div id='client-info' className='flex flex-col justify-center'>
-
-
+          <div id='client-info' className='flex flex-col justify-center items-center lg:items-start gap-4 w-[70%]'>
+            <h1 className='text-[28px] font-bold text-black text-center'>Linda Larson <span className='text-[28px] text-orange-500 italic font-semibold'>designer</span></h1>
+            <p className='text-lg text-black text-center lg:text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Deserunt, aperiam.</p>
+            <button className='px-10 py-3 bg-orange-600 text-white text-md font-semibold hover:bg-black mt-6'>VIEW MORE</button>
           </div>
-
-
         </div>
-
+        <div className='flex items-center justify-between flex-col lg:flex-row w-full px-4 lg:px-32 gap-10'>
+          <div id='client-img' className='w-[50%] lg:w-[30%]'>
+            <img src={client2} alt='client image' className='w-[600px] h-[300px] lg:w-[350px] lg:h-[400px]' />
+          </div>
+          <div id='client-info' className='flex flex-col justify-center items-center lg:items-start gap-4 w-[70%]'>
+            <h1 className='text-[28px] font-bold text-black text-center'>Andrew Dami<span className='text-[28px] text-orange-500 italic font-semibold'>developer</span></h1>
+            <p className='text-lg text-black text-center lg:text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Deserunt, aperiam.</p>
+            <button className='px-10 py-3 bg-orange-600 text-white text-md font-semibold hover:bg-black mt-6'>VIEW MORE</button>
+          </div>
+        </div>
       </Carousel>
-
     </section>
   )
 }
--- portfolio-website/tailwind.config.js
+++ portfolio-website/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // React dosyalarını burada belirtiyoruz
+    "./src/**/*.{js,jsx,ts,tsx}", // Projenizin React dosyaları
+    "./node_modules/flowbite-react/dist/esm/**/*.{js,jsx}", // Flowbite-React dosyaları
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"), // Flowbite desteği için
+  ],
 };
-
-
