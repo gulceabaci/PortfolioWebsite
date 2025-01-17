80977635adcb928951b1322b531681eb647bbaa9
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-17 16:42:59 +0300

    finished

--- portfolio-website/src/sections/Portfolio.js
+++ portfolio-website/src/sections/Portfolio.js
@@ -11,7 +11,7 @@ import pic8 from '../assets/images/pic8.jpeg';
 
 function Portfolio() {
   return (
-    <section id="portfolio" className="w-full py-32 h-fit flex justify-center items-center gap-1 flex-col">
+    <section id="portfolio" className="w-full py-32 h-fit flex justify-center items-center gap-5 flex-col">
       <h1 className='text-center text-5xl font-bold text-black'>My Works</h1>
       <div id='gallery' className='flex  flex-wrap justify-center items-center gap-8 mt-8'>
         <img src={pic} alt='' className='w-[400px] h-[350px] cursor-pointer'/>
