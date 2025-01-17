4550f4ab80968ca3844a6862ce6e8ae9239bd5d3
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-17 11:38:15 +0300

    Footer.js

--- portfolio-website/src/sections/Footer.js
+++ portfolio-website/src/sections/Footer.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import copyright from '../assets/icons/copyright.svg';
+import totop from '../assets/icons/totop.svg';
+import {Link} from 'react-scroll';
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full bg-black text-white p-6'>
+      <div className='flex justify-center items-center gap-2 text-center'>
+        <img src={copyright} alt='copyright image' width={20} height={20}/>
+        <h1>Copyright 2025 : Gülce ABACI - All Rights Reserved</h1>
+        <Link
+        to='home'
+        spy={true}
+        smooth={true}
+        offset={-100}>
+        <img className='fixed bottom-20 right-3 cursor-pointer' 
+        width={55}
+        height={55}
+        src={totop} 
+        alt='scroll to top image' /></Link>
+
+        </div>
+
+
+    </section>
   )
 }
 
